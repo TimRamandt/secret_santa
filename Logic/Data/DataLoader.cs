550fbc087c92f3386c629bf6b9fd0b0639995a0a
Tim Ramandt <tim.ramandt@hotmail.com>
2018-03-02 21:34:03 +0100

    FileParser

---
+++ Logic/Data/DataLoader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Data {
+    class DataLoader {
+        private string path;
+
+        public DataLoader(string path)
+        {
+            this.path = path;
+        }
+
+
+    }
+}
---
+++ Tests/DataTests/FileParserTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logic.Data;
+
+namespace Tests.DataTests {
+    [TestClass]
+    public class FileParserTests {
+
+        [TestMethod]
+        public void TestCorrectInput()
+        {
+            string content = 
+@"Name: My Test
+Tim <someEmail@hotmail.com>
+Tom <another@Email.com>
+Bart <bartje@gmail.com>";
+
+            var list = FileParser.ParseList(content);
+
+            Assert.AreEqual("My Test", list.Name);
+            Assert.AreEqual(3, list.Count);
+            Assert.AreEqual(list[1].Name, "Tom");
+            Assert.AreEqual(list[1].Email, "another@Email.com");
+            
+        }
+
+
+    }
+}
